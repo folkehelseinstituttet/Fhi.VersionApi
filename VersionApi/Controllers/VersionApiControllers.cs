568ede1557b57664e5c2608def9c2ff3c918e08c
Isabel <isabel@hermit.no>
2022-03-09 10:47:44 +0100

    init

---
+++ VersionApi/Controllers/VersionApiControllers.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VersionApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VersionApiControllers : ControllerBase
+    {
+        static Dictionary<string, VersionDTO> information = new();
+
+        [HttpGet("GetInformation")]
+        public ActionResult GetInformation(string system, string component)
+        {
+            var dtoFound = information.TryGetValue($"{system}.{component}", out var dto);
+           
+            if (dtoFound == false)
+            {
+                return Ok(new ShieldsIo("Version", "Not Found"));
+            }
+            else
+            {
+                return Ok(new ShieldsIo("Version", dto!.Version));
+            }
+
+        }
+
+        [HttpGet("SetInformation")]
+        public void SetInformation(string system, string component, string version)
+        {
+            VersionDTO dto = new()
+            {
+                System = system,
+                Component = component,
+                Version = version
+            };
+            information.Add($"{system}.{component}", dto);
+        }
+    }
+
+    /// <summary>
+    /// To be used with Badges in Shields.io, ref https://shields.io/endpoint
+    /// </summary>
+    public class ShieldsIo
+    {
+        public int schemaVersion => 1;
+        public string label { get; set; }
+        public string message { get; set; }
+
+        public string color { get; set; } = "lightgrey";
+
+        public ShieldsIo(string label, string message)
+        {
+            this.label = label;
+            this.message = message;
+        }
+    }
+
+}
+
---
+++ VersionApi/VersionDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace VersionApi
+{
+    public class VersionDTO
+    {
+        public string System { get; set; } = "A";
+
+        public string Component { get; set; } = "B";
+
+        public string Version { get; set; }= "1.2.3";
+
+    }
+}
